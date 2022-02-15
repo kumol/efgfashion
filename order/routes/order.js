0ee7ee42b502eb68fb5ed721ef74cf7d828f4fa3
kumol <kumol01763@gmail.com>
2022-02-16 03:01:56 +0600

    order Permission

--- order/routes/order.js
+++ order/routes/order.js
@@ -1,11 +1,11 @@
 const route = require("express").Router();
-const { Customer } = require("../../common/middleware/Permission");
+const { Customer, Admin } = require("../../common/middleware/Permission");
 const OrderController = require("../controller/Order");
 
-route.get("/", OrderController.getAllOrder);
+route.get("/", Admin, OrderController.getAllOrder);
 route.post("/", Customer, OrderController.placeOrder);
-route.get("/:id", OrderController.getSingleOrder);
-route.put("/:id", OrderController.updateOrder);
-route.delete("/:id", OrderController.deleteOrder);
+route.get("/:id", Customer, OrderController.getSingleOrder);
+route.put("/:id", Customer, OrderController.updateOrder);
+route.delete("/:id", Customer, OrderController.deleteOrder);
 
 module.exports = route;
