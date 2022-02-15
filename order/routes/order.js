d85f8283b63e20fd42e2b97e14ddb1fabb3701da
kumol <kumol01763@gmail.com>
2022-02-15 21:19:46 +0600

    order route

--- order/routes/order.js
+++ order/routes/order.js
@@ -1,8 +1,9 @@
 const route = require("express").Router();
+const { Customer } = require("../../common/middleware/Permission");
 const OrderController = require("../controller/Order");
 
 route.get("/", OrderController.getAllOrder);
-route.post("/", OrderController.placeOrder);
+route.post("/", Customer, OrderController.placeOrder);
 route.get("/:id", OrderController.getSingleOrder);
 route.put("/:id", OrderController.updateOrder);
 route.delete("/:id", OrderController.deleteOrder);
